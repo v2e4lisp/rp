27984d01c20072e32690e090ee073ed8a98aaca5
wenjun.yan <mylastnameisyan@gmail.com>
2013-07-27 02:08:04 +0900

    0.0.1

--- lib/rp.rb
+++ lib/rp.rb
@@ -1,5 +1,43 @@
 require "rp/version"
+require "Rhtml"
 
 module Rp
-  # Your code goes here...
+  extend self
+
+  INDENT_SIZE = 2
+  INDENT = " " * INDENT_SIZE
+
+  def indent(line)
+    (line.size - line.lstrip.size)/INDENT_SIZE
+  end
+
+  def ends s, n=0
+    n.times {|i| @doc << "\n" << INDENT*(s-i) << "end"} if s > 0
+  end
+
+  def parse(lines)
+    @doc = ""
+    @indent = 0
+    lines.each do |l|
+      l.chomp!
+      next if l.empty?
+      i = indent l
+
+      if i > @indent
+        # do
+        @doc << " do\n" << l
+        @indent = i
+      elsif i < @indent
+        # end
+        ends @indent-1, @indent-i
+        @indent = i
+        @doc << "\n" << l
+      else
+        @doc << "\n" << l
+      end
+    end
+
+    ends(@indent - 1, @indent)
+    eval(@doc)
+  end
 end
--- rp.gemspec
+++ rp.gemspec
@@ -8,9 +8,9 @@ Gem::Specification.new do |spec|
   spec.version       = Rp::VERSION
   spec.authors       = ["wenjun.yan"]
   spec.email         = ["mylastnameisyan@gmail.com"]
-  spec.description   = %q{TODO: Write a gem description}
-  spec.summary       = %q{TODO: Write a gem summary}
-  spec.homepage      = ""
+  spec.description   = %q{a better way to write html}
+  spec.summary       = %q{a tiny language that compiles to html . Based on Rhtml}
+  spec.homepage      = "https://github.com/v2e4lisp/rp"
   spec.license       = "MIT"
 
   spec.files         = `git ls-files`.split($/)
@@ -20,4 +20,5 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
+  spec.add_development_dependency "Rhtml"
 end
