035a1a3908df310767866a8dd0f82716f4d7b4ad
wenjun.yan <wenjun.yan@gree.co.jp>
2013-07-30 12:00:51 +0900

    something wrong

--- lib/rp.rb
+++ lib/rp.rb
@@ -17,6 +17,33 @@ module Rp
     n.times {|i| @doc << "\n" << INDENT*(s-i) << "end"} if s >= 0
   end
 
+  def class_and_id l, i
+    cls = ""
+    id = ""
+
+    parts = l.split(" ", 1)
+    t1, t2 = parts[0].split("#")
+    tmp = t1.split(".")
+    tag_name = tmp.shift
+    cls << tmp.join(" ").strip if tmp.any?
+    if t2
+      tmp = t2.split(".")
+      id = tmp.shift.strip
+      cls << " " << tmp.join(" ").strip if tmp.any?
+    end
+
+    l = if parts[1]
+          tag_name + " " + parts[1]
+        else
+          tag_name
+        end
+
+    t = ""
+    t << "#{INDENT*i}@class_and_id[:class] = '#{cls}'\n" if cls.size != 0
+    t << "#{INDENT*i}@class_and_id[:id] = '#{id}'\n"  if id.size != 0
+    t << INDENT*i << l
+  end
+
   def parse(lines)
     @doc = ""
     @indent = 0
@@ -30,6 +57,8 @@ module Rp
         @doc << "\n#{l}"
         next
       end
+
+      # l = class_and_id l, i
 
       if i > @indent
         # do
@@ -49,3 +78,5 @@ module Rp
     eval(@doc)
   end
 end
+
+print Rp.parse(File.open(File.expand_path("../tmp/test.r")).readlines)
